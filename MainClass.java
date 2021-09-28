f4864e15540de4281987c072ff8d21e7840baadd
malharjoshi <malharjoshi95@gmail.com>
2021-09-28 15:57:28 -0400

    Changed to java

---
+++ MainClass.java
@@ -0,0 +1,7 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class MainClass{
+    public static void main(String[] args) {
+        System.out.println("There there");
+    }
+}
--- src/main.py
+++
@@ -1,2 +0,0 @@
-#random file
-#ISPROJECT1
